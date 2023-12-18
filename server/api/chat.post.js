485f22e936355426521697b84ef54c3277ca7b16
Namrata Sivakumar <43491187+namratasiv@users.noreply.github.com>
2023-12-18 21:50:40 +0530

    Update chat.post.js

--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -26,28 +26,6 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   console.log(result);
   console.log(response);
 
-
-
-	// const req = await fetch('https://api.openai.com/v1/completions', {
-	// 	method: 'POST',
-	// 	headers: {
-	// 		'Content-Type': 'application/json',
-	// 		Authorization: `Bearer ${config.OPENAI_API_KEY}`
-	// 	},
-	// 	body: JSON.stringify({
-	// 		model: 'text-davinci-003',
-	// 		prompt: prompt,
-	// 		temperature: 0.9,
-	// 		max_tokens: 512,
-	// 		top_p: 1.0,
-	// 		frequency_penalty: 0,
-	// 		presence_penalty: 0.6,
-	// 		stop: [' User:', ' AI:']
-	// 	})
-	// });
-
-	// const res = await req.json();
-	// const result = res.choices[0];
 	return {
 		message: text
 	};
